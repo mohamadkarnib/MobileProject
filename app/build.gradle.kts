1ea6d80a1a73adc2c12628f34c454d2d32746f89
Mohamad Karnib <mhamadkarnib88@email.com>
2025-05-08 22:58:21 +0300

    Updated compileSdkVersion to 35

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,12 +4,12 @@ plugins {
 
 android {
     namespace = "com.example.project"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.project"
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
